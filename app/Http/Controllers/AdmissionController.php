771c41f55c827c6fc3fba8797c6747dfa1a604e3
Awa425 <diopawa192@gmail.com>
2025-03-12 01:29:48 +0000

    test ci

--- app/Http/Controllers/AdmissionController.php
+++ app/Http/Controllers/AdmissionController.php
@@ -40,6 +40,14 @@ class AdmissionController extends Controller
     return response()->json($admission);
     }
 
+    public function indexTest()
+    {
+    $admission = Admission::with('dossierMedical.patient','service','salle')
+    ->orderBy('id', 'DESC')
+    ->get();
+    return response()->json($admission);
+    }
+
     /**
      * @OA\Post(
      *     path="/api/admissions/en-cours",
